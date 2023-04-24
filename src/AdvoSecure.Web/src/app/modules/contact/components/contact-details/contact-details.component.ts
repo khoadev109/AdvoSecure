1ce4d3863a6a26d2e01f88b565e4fab69e7d0417
nthanhtam <tam.nguyen1@tekmedi.vn>
2023-04-24 12:41:48 +0700

    Add more field in contact.model.ts and add  button visiting address

--- src/AdvoSecure.Web/src/app/modules/contact/components/contact-details/contact-details.component.ts
+++ src/AdvoSecure.Web/src/app/modules/contact/components/contact-details/contact-details.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, NgModule } from '@angular/core';
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { ContactService } from '../../services/contact.service';
 import { Router } from '@angular/router';
@@ -37,6 +37,25 @@ export class ContactDetailsComponent implements OnInit {
 
   imageFile: File;
 
+  sameAsPostalAddress: boolean;
+  streetPostbox1: '';
+  streetPostbox2: '';
+  addressModel1: '';
+  addressModel2: '';
+  postCode1: '';
+  postCode2: '';
+  houseNumber1: '';
+  houseNumber2: '';
+  place1: '';
+  place2: '';
+  provinceState1: '';
+  provinceState2: '';
+  addition1: '';
+  addition2: '';
+  country1: '';
+  country2: '';
+  isChecked: false;
+
   contact: Contact = {
     displayName: '',
     bankAccount: '',
@@ -45,6 +64,76 @@ export class ContactDetailsComponent implements OnInit {
     sepaMandateNumber: '',
     sepaMandateDate: new Date(),
     sepaMandateLimit: 0,
+    middleName: '',
+    initials: '',
+    onlineStatusId: 0,
+    email1DisplayName: '',
+    email1EmailAddress: '',
+    email2DisplayName: '',
+    email2EmailAddress: '',
+    email3DisplayName: '',
+    email3EmailAddress: '',
+    fax1DisplayName: '',
+    fax1FaxNumber: '',
+    fax2DisplayName: '',
+    fax2FaxNumber: '',
+    fax3DisplayName: '',
+    fax3FaxNumber: '',
+    address1DisplayName: '',
+    address1AddressStreet: '',
+    address1AddressHouseNo: '',
+    address1AddressHouseNoExt: '',
+    address1AddressLine2: '',
+    address1AddressCity: '',
+    address1AddressPostalCode: '',
+    address1AddressCountry: '',
+    address1AddressCountryCode: '',
+    address2AddressStreet: '',
+    address2AddressCity: '',
+    address2AddressHouseNoExt: '',
+    address2AddressHouseNo: '',
+    address2AddressLine2: '',
+    address2AddressStateOrProvince: '',
+    address2AddressPostalCode: '',
+    address2AddressCountry: '',
+    address2AddressCountryCode: '',
+    address1AddressPostOfficeBox: '',
+    address2DisplayName: '',
+    address2AddressPostOfficeBox: '',
+    address3DisplayName: '',
+    address3AddressStreet: '',
+    address3AddressHouseNo: '',
+    address3AddressHouseNoExt: '',
+    address3AddressLine2: '',
+    address3AddressCity: '',
+    address3AddressStateOrProvince: '',
+    address3AddressPostalCode: '',
+    address3AddressCountry: '',
+    address3AddressCountryCode: '',
+    address3AddressPostOfficeBox: '',
+    telephone3DisplayName: '',
+    telephone3TelephoneNumber: '',
+    telephone4DisplayName: '',
+    telephone4TelephoneNumber: '',
+    telephone5DisplayName: '',
+    telephone5TelephoneNumber: '',
+    telephone6DisplayName: '',
+    telephone6TelephoneNumber: '',
+    telephone7DisplayName: '',
+    telephone7TelephoneNumber: '',
+    telephone8DisplayName: '',
+    telephone8TelephoneNumber: '',
+    telephone9DisplayName: '',
+    telephone10DisplayName: '',
+    telephone10TelephoneNumber: '',
+    departmentName: '',
+    officeLocation: '',
+    managerName: '',
+    assistantName: '',
+    instantMessagingAddress: '',
+    personalHomePage: '',
+    businessHomePage: '',
+    socialSecurityNumber: '',
   };
 
   email: FormGroup;
@@ -56,11 +145,22 @@ export class ContactDetailsComponent implements OnInit {
 
   selectedIdTypeId = 0;
   selectedMaritalStatusId = 0;
-  selectedCountryId = '';
+  selectedCountryId1 = '';
+  selectedCountryId2 = '';
   selectedCountryVaId = '';
   selectedGender = '';
+  date = '';
 
   activeTab: Tabs = 'address-tab';
+
+  dateOfBirth: Date = new Date();
+  today: Date = new Date();
+  maxDate =
+    new Date().getFullYear().toString() +
+    '-0' +
+    (new Date().getMonth() + 1).toString() +
+    '-' +
+    new Date().getDate().toString();
 
   constructor(
     private router: Router,
@@ -151,6 +251,55 @@ export class ContactDetailsComponent implements OnInit {
       this.changeDetectorRef.detectChanges();
     }
   };
+
+  updateInput() {
+    if (this.isChecked) {
+      this.streetPostbox2 = this.streetPostbox1;
+      this.addressModel2 = this.addressModel1;
+      this.postCode2 = this.postCode1;
+      this.houseNumber2 = this.houseNumber1;
+      this.place2 = this.place1;
+      this.provinceState2 = this.provinceState1;
+      this.addition2 = this.addition1;
+      this.selectedCountryId2 = this.selectedCountryId1;
+    } else {
+      this.streetPostbox2 = '';
+      this.addressModel2 = '';
+      this.postCode2 = '';
+      this.houseNumber2 = '';
+      this.place2 = '';
+      this.provinceState2 = '';
+      this.addition2 = '';
+      this.selectedCountryId2 = '';
+    }
+  }
+
+  goToAll() {
+    this.router.navigate(['/management/contacts/all']);
+  }
+
+  isValidDateOfBirth(dateOfBirth: string): boolean {
+    const date = new Date(dateOfBirth);
+
+    if (isNaN(date.getTime())) {
+      return false;
+    }
+
+    const now = new Date();
+    const diff = now.getTime() - date.getTime();
+
+    const age = diff / 1000 / 60 / 60 / 24 / 365.25;
+    return age >= 18;
+  }
+
+  dateChange(event: any) {
+    const dob = event.target?.value;
+    if (this.isValidDateOfBirth(dob)) {
+      this.date = 'Valid date of birth';
+    } else {
+      this.date = 'Invalid date of birth';
+    }
+  }
 
   ngOnDestroy() {
     this.unsubscribe.forEach((sb) => sb.unsubscribe());
